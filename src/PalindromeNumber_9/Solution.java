ffe82b6f6f4ac2b7e4188b929378d7b2a1bfc52a
Bibek Ghimire <bibekghimire773@gmail.com>
2026-08-14 00:50:49 +0545

    feat: solution to palindrome number added

---
+++ src/PalindromeNumber_9/Solution.java
@@ -0,0 +1,23 @@
+package PalindromeNumber_9;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x< 0){
+            return false;
+        }
+        else{
+            int temp = x, rev =0;
+
+            while(temp !=0){
+                rev = (rev*10)+(temp%10);
+                temp = temp/10;
+            }
+            if(rev == x){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
